8c7c9b3a50afa7a93fa33467dec5a3cc6fbba31b
Rahulchoudhary17 <73299986+Rahulchoudhary17@users.noreply.github.com>
2022-03-13 00:07:10 +0530

    Update config.py

--- Aviax/config.py
+++ Aviax/config.py
@@ -36,7 +36,7 @@ class Config(object):
     URL = None
     STRICT_GBAN = False
     MONGO_DB_URL = "mongodb+srv://sofia:sofia@cluster0.axmal.mongodb.net/sofia?retryWrites=true&w=majority"
-    REDIS_URL = "redis://AVIAX:145Qjnt2wbsrStZTsrDIfhwkydfarqYZ@redis-18861.c91.us-east-1-3.ec2.cloud.redislabs.com:18861/AVIAX"
+    REDIS_URL = "redis://ItzWorldsEnd_Xo:Papasepooch_1@redis-18861.c91.us-east-1-3.ec2.cloud.redislabs.com:18861/ItzWorldsEnd_Xo"
     
     SPAMWATCH_SUPPORT_CHAT = "@AviaxSupport"
     SPAMWATCH_API = "xpKDzGXxXksbRFYrQGKH3445Ex0fEMegtptlwXTIMoKXuZ2NpZZt1aLQji8kgeEm"
