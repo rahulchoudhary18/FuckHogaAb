38f4457c754853a11f273528f9a9cc32010e61eb
Rahulchoudhary17 <73299986+Rahulchoudhary17@users.noreply.github.com>
2022-03-12 12:50:50 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Cutiepii Robot
-:: Enables virtual env mode and then starts Cutiepii
-env\scripts\activate.bat && py -m Cutiepii_Robot
+TITLE Aviax Beatz
+:: Enables virtual env mode and then starts Aviax
+env\scripts\activate.bat && py -m Aviax
