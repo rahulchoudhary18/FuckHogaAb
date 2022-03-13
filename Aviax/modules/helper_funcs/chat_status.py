992bf8bbcc11bb3af3e051e23fcaedcb610ca813
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-03-13 11:46:03 +0530

    Update chat_status.py

--- Aviax/modules/helper_funcs/chat_status.py
+++ Aviax/modules/helper_funcs/chat_status.py
@@ -72,6 +72,9 @@ def user_can_promote(chat: Chat, user: User, bot_id: int) -> bool:
 def user_can_pin(chat: Chat, user: User, bot_id: int) -> bool:
     return chat.get_member(user.id).can_pin_messages
 
+def user_can_manage_voice_chats(chat: Chat, user: User, bot_id: int) -> bool:
+    return chat.get_member(user.id).can_manage_voice_chats
+
 def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
     if (
         chat.type == "private"
