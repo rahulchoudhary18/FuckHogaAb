2f2ff41bb61cc10c04ac5d366c711f72b9dfc66f
Rahulchoudhary17 <73299986+Rahulchoudhary17@users.noreply.github.com>
2022-04-11 18:31:15 +0530

    Update chatbot_sql.py

--- Aviax/modules/sql/chatbot_sql.py
+++ Aviax/modules/sql/chatbot_sql.py
@@ -1,72 +1,73 @@
-"""
-MIT License
-
-Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2021 Awesome-RJ
-Copyright (c) 2021, Yūki • Black Knights Union, <https://github.com/Awesome-RJ/CutiepiiRobot>
-
-This file is part of @Cutiepii_Robot (Telegram Bot)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import threading
 
-from sqlalchemy import Column, String
 from Aviax.modules.sql import BASE, SESSION
+from sqlalchemy import Column, String
 
-class KukiChats(BASE):
-    __tablename__ = "kuki_chats"
+
+class ChatbotChats(BASE):
+    __tablename__ = "chatbot_chats"
     chat_id = Column(String(14), primary_key=True)
+    ses_id = Column(String(70))
+    expires = Column(String(15))
 
-    def __init__(self, chat_id):
+    def __init__(self, chat_id, ses_id, expires):
         self.chat_id = chat_id
+        self.ses_id = ses_id
+        self.expires = expires
 
-KukiChats.__table__.create(checkfirst=True)
+
+ChatbotChats.__table__.create(checkfirst=True)
+
 INSERTION_LOCK = threading.RLock()
 
 
-def is_kuki(chat_id):
+def is_chat(chat_id):
     try:
-        chat = SESSION.query(KukiChats).get(str(chat_id))
-        return bool(chat)
+        chat = SESSION.query(ChatbotChats).get(str(chat_id))
+        if chat:
+            return True
+        else:
+            return False
     finally:
         SESSION.close()
 
-def set_kuki(chat_id):
+
+def set_ses(chat_id, ses_id, expires):
     with INSERTION_LOCK:
-        kukichat = SESSION.query(KukiChats).get(str(chat_id))
-        if not kukichat:
-            kukichat = KukiChats(str(chat_id))
-        SESSION.add(kukichat)
+        autochat = SESSION.query(ChatbotChats).get(str(chat_id))
+        if not autochat:
+            autochat = ChatbotChats(str(chat_id), str(ses_id), str(expires))
+        else:
+            autochat.ses_id = str(ses_id)
+            autochat.expires = str(expires)
+
+        SESSION.add(autochat)
         SESSION.commit()
 
-def rem_kuki(chat_id):
+
+def get_ses(chat_id):
+    autochat = SESSION.query(ChatbotChats).get(str(chat_id))
+    sesh = ""
+    exp = ""
+    if autochat:
+        sesh = str(autochat.ses_id)
+        exp = str(autochat.expires)
+
+    SESSION.close()
+    return sesh, exp
+
+
+def rem_chat(chat_id):
     with INSERTION_LOCK:
-        kukichat = SESSION.query(KukiChats).get(str(chat_id))
-        if kukichat:
-            SESSION.delete(kukichat)
+        autochat = SESSION.query(ChatbotChats).get(str(chat_id))
+        if autochat:
+            SESSION.delete(autochat)
+
         SESSION.commit()
 
 
-def get_all_kuki_chats():
+def get_all_chats():
     try:
-        return SESSION.query(KukiChats.chat_id).all()
+        return SESSION.query(ChatbotChats.chat_id).all()
     finally:
         SESSION.close()
